32a937e960fcd658b1728d6ab817b9405b5a94a2
Koray Ozkal <koryozkal@gmail.com>
2020-04-15 08:04:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(phrase)
-if phrase = "I LOVE YOU GRANDMA!"
+if phrase.upcase = "I LOVE YOU GRANDMA!"
     puts 'I LOVE YOU TOO PUMPKIN!'
 end
+end 
 
 # Write a speak_to_grandma method.
 
